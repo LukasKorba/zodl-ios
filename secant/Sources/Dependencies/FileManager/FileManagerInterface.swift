c606728d231073c15d6fa855848a74e0de9eacd9
Michal Fousek <chlup@chlup.info>
2026-04-10 20:51:16 +0200

    Make FileManagerClient Swift 6 compliant

--- secant/Sources/Dependencies/FileManager/FileManagerInterface.swift
+++ secant/Sources/Dependencies/FileManager/FileManagerInterface.swift
@@ -8,17 +8,7 @@
 import Foundation
 
 struct FileManagerClient {
-    let url: (FileManager.SearchPathDirectory, FileManager.SearchPathDomainMask, URL?, Bool) throws -> URL
-    let fileExists: (String) -> Bool
-    let removeItem: (URL) throws -> Void
-    
-    init(
-        url: @escaping (FileManager.SearchPathDirectory, FileManager.SearchPathDomainMask, URL?, Bool) throws -> URL,
-        fileExists: @escaping (String) -> Bool,
-        removeItem: @escaping (URL) throws -> Void)
-    {
-        self.url = url
-        self.fileExists = fileExists
-        self.removeItem = removeItem
-    }
+    var url: @Sendable (FileManager.SearchPathDirectory, FileManager.SearchPathDomainMask, URL?, Bool) throws -> URL
+    var fileExists: @Sendable (String) -> Bool = { _ in false }
+    var removeItem: @Sendable (URL) throws -> Void
 }
